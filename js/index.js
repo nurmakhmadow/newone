b3a5d620c064092512ac5bddae3317ef9330c68a
nurmakhmadow <nurmakhmadow@mail.ru>
2023-06-06 17:06:13 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+
+let studio = arr.filter(item => {
+    if (Number.length >= 5){
+ console.log(
+    'good'
+ );
+    
+    } else {
+        console.log(
+            'bad'
+        );
+    }
+
+})
